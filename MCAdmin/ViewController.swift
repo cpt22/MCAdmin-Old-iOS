7fc3f14500f59f641a4a2f3a7e3590b19fed8b00
Christian <ctingle18@gmail.com>
2020-04-05 17:33:48 -0500

    Initial Commit

---
+++ MCAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MCAdmin
+//
+//  Created by Christian Tingle on 4/5/20.
+//  Copyright © 2020 Christian Tingle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
